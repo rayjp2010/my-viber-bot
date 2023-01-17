b12def9ee02c5e2b8514a7b56097ab7e12912ae8
rayjp2010 <rayjp2010@gmail.com>
2023-01-17 17:48:52 +0900

    test

--- index.js
+++ index.js
@@ -17,55 +17,22 @@ console.log(`name: ${process.env.BOT_NAME}`)
 console.log(`avatar: ${process.env.BOT_AVATAR_URL}`)
 console.log(`hook: ${process.env.WEBHOOK_URL}`)
 
-// const logger = createLogger();
-// const app = express()
-//
-// const bot = new ViberBot({
-//     authToken,
-//     name,
-//     avatar, // It is recommended to be 720x720, and no more than 100kb.
-//     logger,
-// });
-//
-// function createLogger() {
-//     const logger = winston.createLogger({
-//         level: "debug"
-//     }); // We recommend DEBUG for development
-//     return logger;
-// }
-//
-// // Perfect! Now here's the key part:
-// bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
-//     // Echo's back the message to the client. Your bot logic should sit here.
-//     response.send(message);
-// });
-//
-// // A simple regular expression to answer messages in the form of 'hi' and 'hello'.
-// bot.onTextMessage(/^hi|hello$/i, (message, response) => {
-//     console.log('test')
-//     response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`))
-// })
-//
-// // app.use('/viber/webhook', bot.middleware())
-//
-// app.get('/', (req, res) => {
-//     console.log('health check')
-//     res.status(200)
-//     res.end('ok')
-// })
-//
-// app.listen(port, () => {
-//     console.log(`Listening on ${port}`)
-//     bot.setWebhook(webhookUrl)
-// });
-
-'use strict';
+const logger = createLogger();
+const app = express()
 
 const bot = new ViberBot({
     authToken,
     name,
-    avatar,
+    avatar, // It is recommended to be 720x720, and no more than 100kb.
+    logger,
 });
+
+function createLogger() {
+    const logger = winston.createLogger({
+        level: "debug"
+    }); // We recommend DEBUG for development
+    return logger;
+}
 
 // Perfect! Now here's the key part:
 bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
@@ -73,7 +40,22 @@ bot.on(BotEvents.MESSAGE_RECEIVED, (message, response) => {
     response.send(message);
 });
 
-// Wasn't that easy? Let's create HTTPS server and set the webhook:
-const https = require('https');
+// A simple regular expression to answer messages in the form of 'hi' and 'hello'.
+bot.onTextMessage(/^hi|hello$/i, (message, response) => {
+    console.log('test')
+    response.send(new TextMessage(`Hi there ${response.userProfile.name}. I am ${bot.name}`))
+})
 
-https.createServer({}, bot.middleware()).listen(port, () => bot.setWebhook(webhookUrl));
+// app.use('/viber/webhook', bot.middleware())
+
+app.get('/', (req, res) => {
+    console.log('health check')
+    res.status(200)
+    res.end('ok')
+})
+
+app.listen(port, () => {
+    console.log(`Listening on ${port}`)
+    bot.setWebhook(webhookUrl)
+});
+
